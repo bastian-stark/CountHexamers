54783c854e8beb0659c3541bba522c54b6fb058c
Bastian Stark <53190965+bastian-stark@users.noreply.github.com>
2024-06-03 09:57:05 -0700

    Update CountHexamers_v2.py

--- CountHexamers_v2.py
+++ CountHexamers_v2.py
@@ -1,6 +1,11 @@
+#CountHexamers V2
+#Bastian Stark
+#May 31, 2024
+#Short script for counting the hexamers from a given BED file.
+
 filename = input('Enter file name: ')
 
-infile = open(f"{filename}")
+infile = open(f"{filename}.fa")
 outfile = open(f"{filename}_counts.txt", "w")
 sequencelist = []
 for line in infile:
@@ -10,6 +15,7 @@ for line in infile:
 
 sequencelist.sort()
 sequencecounts = {}
+#creates a dictionary to store hexamer counts, adds hexamers to dictionary if not already present
 for line in sequencelist:
     if line not in sequencecounts:
         sequencecounts[line] = 1
